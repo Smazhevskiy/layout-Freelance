fa515cedc8f28c2c0fc8241ca2ba84dfd5f5d02f
user_nitro_5 <timmy.lepel@gmail.com>
2022-01-05 21:04:15 +0300

    slider block done

--- js/main.js
+++ js/main.js
@@ -1,15 +1,10 @@
 $(function () {
 
- $('.slider__inner, .news__slider-inner').slick({
+ $('.slider__inner').slick({
   nextArrow: '<button type="button" class="slick-btn slick-next"></button>',
   prevArrow: '<button type="button" class="slick-btn slick-prev"></button>',
   infinite: false,
  });
 
- $('select').styler();
-
- $('.header__btn-menu').on('click', function() {
-  $('.menu ul').slideToggle();
- })
 
 });
